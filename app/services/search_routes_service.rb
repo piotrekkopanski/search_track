aab9491c10531df9493372652f44fb6a31812576
piotrkopanski <piotrkopanski@interia.pl>
2018-04-19 22:32:34 +0200

    style of code

--- app/services/search_routes_service.rb
+++ app/services/search_routes_service.rb
@@ -1,7 +1,7 @@
 # Service to search routes
 class SearchRoutesService
   def self.check_arguments(route_type, start_city_id, finish_city_id)
-  	@error_messages = []
+    @error_messages = []
     @error_messages.push('Route type must  be driver or passenger') unless route_type.in? %w[driver passenger].map(&:freeze).freeze
     @error_messages.push('Start City must exist') if RoutePoint.pluck(:city_id).exclude?(start_city_id)
     @error_messages.push('Finish City must exist') if RoutePoint.pluck(:city_id).exclude?(finish_city_id)
